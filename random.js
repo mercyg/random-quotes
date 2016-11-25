3d9bc7b620b6161aaa98dcb9ff8d217249e93700
Meheret Egzerab <meheretgirma22@gmail.com>
2016-11-25 14:55:28 -0500

    fixed http

--- random.js
+++ random.js
@@ -5,21 +5,24 @@
 
     $("#btn").click(function () {
         getQuotes();
+        //console.log(getQuotes)
     });
    var autor;
     var qutote;
      var quoteslink;
     function getQuotes() {
-        $.get("//api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function (data) {
-            // console.log(data.quoteText);
-             autor = data.quoteAuthor
+        $.get("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function (data) {
+            console.log(data.quoteAuthor);
+             autor = data.quoteAuthor;
+             //console.log(autor)
              qutote = data.quoteText;
              quoteslink = data.quoteLink;
             $("#quotes").html(qutote);
             $("#authors").html(autor);
             $("#link").attr("href", quoteslink).html(quoteslink);
-
+     
         })
+        
     }
 
     $("#tweet").click(function () {
